9856f74edd2a57243eab12038a50c962eee5fffa
McLaren <artyudin@outlook.com>
2017-03-20 22:17:35 -0400

    March 20th

--- web_project/record_app/urls.py
+++ web_project/record_app/urls.py
@@ -4,5 +4,6 @@ from record_app import views
 urlpatterns = [
    
     url(r'^$', views.ListPostsView.as_view(), name='list_posts'),
+    url(r'^(?P<pk>\d+)/$', views.DetailPostView.as_view(), name='detail_posts'),
 
 ]
--- web_project/record_app/views.py
+++ web_project/record_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from record_app.models import Post
 
 
@@ -7,3 +8,11 @@ from record_app.models import Post
 class ListPostsView(ListView):
 
     model = Post
+
+
+
+class DetailPostView(DetailView):
+
+	model = Post
+
+	
